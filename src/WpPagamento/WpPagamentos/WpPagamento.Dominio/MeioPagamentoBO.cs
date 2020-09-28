d9d6861a1b8951c535b46f1bb66bdcf214b5ca5e
lucas fernando <Luca-max@hotmail.com>
2020-09-28 08:46:43 -0300

    Atualização de pagamentos

---
+++ src/WpPagamento/WpPagamentos/WpPagamento.Dominio/MeioPagamentoBO.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpPagamentos.Entidade;
+using WpPagamentos.Servico;
+using Rep = wpPagamentos.Repositorio;
+namespace WpPagamento.Dominio
+{
+    public static class MeioPagamentoBO
+    {
+        /// <summary>
+        /// Metodo de salvar MeioPagamento (Async)
+        /// </summary>
+        /// <param name="MeioPagamento"> Objeto Produtp</param>
+        /// <param name="token"> Token valido</param>
+        /// <returns>Verdadeiro: Salvou o MeioPagamento / Falso: Houve falha</returns>
+        public static async Task<object> SaveAsync(MeioPagamento MeioPagamento, string token)
+        {
+            if (await SeguracaServ.validaTokenAsync(token))
+            {
+                Rep.MeioDePagamento rep = new Rep.MeioDePagamento();
+                if (MeioPagamento.idCliente != 0)
+                    try { return rep.Add(MeioPagamento); } catch { return false; }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Metodo de retornar todos os MeioPagamentos por cliente
+        /// </summary>
+        /// <param name="idCliente">ID do cliente solicitante</param>
+        /// <param name="token">Token Valido</param>
+        /// <returns></returns>
+        public static async Task<List<MeioPagamento>> GetAllAsync(int idCliente, string token)
+        {
+            Rep.MeioDePagamento MeioPagamentoRep = new Rep.MeioDePagamento(); 
+
+            if (await SeguracaServ.validaTokenAsync(token))
+                return MeioPagamentoRep.GetAll().Where(x => x.idCliente == idCliente).ToList();
+            else
+                return new List<MeioPagamento>();
+        }
+
+        /// <summary>
+        /// Metodo de deletar MeioPagamento
+        /// </summary>
+        /// <param name="MeioPagamento">MeioPagamento que iraser deletado</param>
+        /// <param name="token">Token valido</param>
+        /// <returns>Verdadeiro: Removeu o MeioPagamento / Falso: Houve falha</returns>
+        public static async Task<object> RemoveAsync(Object MeioPagamento, string token)
+        {
+            dynamic objEn = MeioPagamento;
+            string a = objEn.ID.ToString();
+            if (await SeguracaServ.validaTokenAsync(token))
+            {
+                Rep.MeioDePagamento MeioPagamentoRep = new Rep.MeioDePagamento();
+
+                MeioPagamento obj = MeioPagamentoRep.GetAll().Where(x => x.ID == Convert.ToInt32(a)).FirstOrDefault();
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ src/WpPagamento/WpPagamentos/WpPagamento.Dominio/PropriedadeBO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpPagamentos.Entidade;
+using WpPagamentos.Servico;
+using Rep = wpPagamentos.Repositorio;
+
+namespace WpPagamento.Dominio
+{
+    public static class PropriedadesBO
+    {
+        /// <summary>
+        /// Metodo de salvar Propriedades (Async)
+        /// </summary>
+        /// <param name="Propriedades"> Objeto Produtp</param>
+        /// <param name="token"> Token valido</param>
+        /// <returns>Verdadeiro: Salvou o Propriedades / Falso: Houve falha</returns>
+        public static async Task<object> SaveAsync(Propriedades Propriedades, string token)
+        {
+            if (await SeguracaServ.validaTokenAsync(token))
+            {
+                Rep.Propriedades rep = new Rep.Propriedades();
+                if (Propriedades.idCliente != 0)
+                    try { return rep.Add(Propriedades); } catch { return false; }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Metodo de retornar todos os Propriedadess por cliente
+        /// </summary>
+        /// <param name="idCliente">ID do cliente solicitante</param>
+        /// <param name="token">Token Valido</param>
+        /// <returns></returns>
+        public static async Task<List<Propriedades>> GetAllAsync(int idCliente, string token)
+        {
+            Rep.Propriedades PropriedadesRep = new Rep.Propriedades();
+
+            if (await SeguracaServ.validaTokenAsync(token))
+                return PropriedadesRep.GetAll().Where(x => x.idCliente == idCliente).ToList();
+            else
+                return new List<Propriedades>();
+        }
+
+        /// <summary>
+        /// Metodo de deletar Propriedades
+        /// </summary>
+        /// <param name="Propriedades">Propriedades que iraser deletado</param>
+        /// <param name="token">Token valido</param>
+        /// <returns>Verdadeiro: Removeu o Propriedades / Falso: Houve falha</returns>
+        public static async Task<object> RemoveAsync(Object Propriedades, string token)
+        {
+            dynamic objEn = Propriedades;
+            string a = objEn.ID.ToString();
+            if (await SeguracaServ.validaTokenAsync(token))
+            {
+                Rep.Propriedades PropriedadesRep = new Rep.Propriedades();
+
+                Propriedades obj = PropriedadesRep.GetAll().Where(x => x.ID == Convert.ToInt32(a)).FirstOrDefault();
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
